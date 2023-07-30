7d950ff62e431746ca421b71146a935107ddf2e5
yilverdeja <yilverdeja@users.noreply.github.com>
2023-07-31 00:20:53 +0800

    feat: add get_hotspot_count.py
    for main.yml github actions

---
+++ get_hotspot_count.py
@@ -0,0 +1,23 @@
+import csv
+SEARCH_CSV_FILES = ["wifi-map-hk.csv"]
+
+def get_count():
+    added = 0
+    not_added = 0
+    for file in SEARCH_CSV_FILES:
+        csv_file = open(file, "r", encoding="utf-8")
+        csv_input = csv.DictReader(csv_file)
+
+        for obj in csv_input:
+            added += 1 if obj["InWifiMap"] == "True" else 0
+            not_added += 0 if obj["InWifiMap"] == "True" else 1
+    
+    return added, not_added
+
+if __name__ == "__main__":
+    added, not_added = get_count()
+    total = added + not_added
+    added_percentage = round(added/total*100, 2)
+    not_added_percentage = round(not_added/total*100, 2)
+
+    print(f"Total: {total}\nAdded: {added} ({added_percentage}%)\nNot Added: {not_added} ({not_added_percentage}%)")
